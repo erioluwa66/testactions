00221baad3aa97c48fbc92cb1f86fc8f56dec403
erioluwa66 <erioluwa66@gmail.com>
2023-09-19 19:50:07 -0400

    mod

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "aws" {}
 
-#resource "aws_vpc" "test" {
-#cidr_block =   "10.0.0.0/16"
-#}
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+
+
+  tags = {
+    Name = "Actions-test"
+  }
+}
+
