2504ec7a1c673eece74cbf84e95bef00cf50e941
erioluwa66 <erioluwa66@gmail.com>
2023-09-20 02:41:49 -0400

    secrets

--- main.tf
+++ main.tf
@@ -9,13 +9,3 @@ resource "aws_vpc" "test" {
     Name = "Actions-test"
   }
 }
-
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.test.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "Actions-test"
-  }
-}
